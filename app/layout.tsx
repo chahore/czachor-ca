2e61327fe50233fbbd5fd61c2751c0ce04c3a83c
David Czachor <david@czachor.dev>
2023-11-09 11:41:24 -0500

    Downgraded next@13.5.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
-
 import './globals.css'
 
 export const metadata: Metadata = {
