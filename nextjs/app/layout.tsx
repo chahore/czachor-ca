af0d6e459dc30b72bd550cf479721f471ca8d604
David Czachor <david@czachor.dev>
2023-11-11 22:20:03 -0500

    Added Vercel Analytics

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -5,6 +5,7 @@ import '@/app/globals.css'
 import SiteHeader from '@/components/global/site-header'
 import SiteFooter from '@/components/global/site-footer'
 import { TailwindIndicator } from '@/components/global/tailwind-indicator'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: {
@@ -24,6 +25,7 @@ export default function RootLayout({
       className={`${GeistSans.variable} ${GeistMono.variable}`}
     >
       <body className="container mx-auto min-h-screen bg-background antialiased sm:max-w-3xl">
+        <Analytics />
         <SiteHeader />
         {children}
         <SiteFooter />
