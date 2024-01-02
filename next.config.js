739fce1031e4bcc8ee614bc432985a7d8835a689
David Czachor <david@czachor.dev>
2024-01-02 13:47:40 -0500

    Updated packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  experimental: {
-    ppr: true,
-  },
+  // experimental: {
+  //   ppr: true,
+  // },
   images: {
     remotePatterns: [
       {
