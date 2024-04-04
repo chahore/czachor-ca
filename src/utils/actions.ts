a8c5ee1fe4aeca01edf87d9922feda8592c96e72
David Czachor <david@czachor.dev>
2024-04-03 22:07:27 -0400

    Removed supabase

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -3,36 +3,36 @@
 import { unstable_noStore as noStore } from 'next/cache'
 import { revalidatePath } from 'next/cache'
 
-import { createClient } from './supabase/server'
+export async function saveWallEntry() {}
 
-const supabase = createClient()
+export async function deleteWallEntry() {}
 
-export async function saveWallEntry({
-  user_message,
-}: {
-  user_message: string
-}) {
-  const { data: session } = await supabase.auth.getUser()
-  await supabase.from('wall_entries').insert({
-    user_message: user_message,
-  })
-  if (!user_message || !session.user?.email)
-    return { error: 'Something went wrong' }
+// export async function saveWallEntry({
+//   user_message,
+// }: {
+//   user_message: string
+// }) {
+//   const { data: session } = await supabase.auth.getUser()
+//   await supabase.from('wall_entries').insert({
+//     user_message: user_message,
+//   })
+//   if (!user_message || !session.user?.email)
+//     return { error: 'Something went wrong' }
 
-  revalidatePath('/wall')
-}
+//   revalidatePath('/wall')
+// }
 
-export async function deleteWallEntry({ id }: { id: number }) {
-  await supabase.from('wall_entries').delete().eq('id', id)
-  revalidatePath('/wall')
-}
+// export async function deleteWallEntry({ id }: { id: number }) {
+//   await supabase.from('wall_entries').delete().eq('id', id)
+//   revalidatePath('/wall')
+// }
 
-export async function fetchWallEntries() {
-  noStore()
-  const result = await supabase
-    .from('wall_entries')
-    .select('*, users(*)')
-    .order('created_at', { ascending: false })
-  console.log(result)
-  return result
-}
+// export async function fetchWallEntries() {
+//   noStore()
+//   const result = await supabase
+//     .from('wall_entries')
+//     .select('*, users(*)')
+//     .order('created_at', { ascending: false })
+//   console.log(result)
+//   return result
+// }
