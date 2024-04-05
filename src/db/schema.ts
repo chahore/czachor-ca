afeac01a7cff82bd5bca50cf7c8040bfe64195e9
David Czachor <david@czachor.dev>
2024-04-05 17:03:03 -0400

    Schema changes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,13 +3,10 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const wallEntries = sqliteTable('wall_entires', {
   id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
-  user_name: text('user_name'),
-  user_email: text('user_email'),
-  user_pic: text('user_picture'),
   user_message: text('user_message').notNull(),
   user_id: text('user_id')
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: 'cascade' }),
   created_at: text('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
@@ -19,6 +16,9 @@ export const userTable = sqliteTable('user', {
   id: text('id').notNull().primaryKey(),
   linkedin_id: integer('linkedin').unique(),
   username: text('username'),
+  user_name: text('user_name'),
+  user_email: text('user_email'),
+  user_pic: text('user_picture'),
 })
 
 export const sessionTable = sqliteTable('session', {
@@ -28,3 +28,11 @@ export const sessionTable = sqliteTable('session', {
     .references(() => userTable.id),
   expiresAt: integer('expires_at').notNull(),
 })
+
+export type InsertWallEntry = typeof wallEntries.$inferInsert
+export type InsertUser = typeof userTable.$inferInsert
+export type InsertSession = typeof sessionTable.$inferInsert
+
+export type SelectWallEntry = typeof wallEntries.$inferSelect
+export type SelectUser = typeof userTable.$inferSelect
+export type SelectSession = typeof sessionTable.$inferSelect
