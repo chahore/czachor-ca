bbdc96caa4c7246b7e853d07080474cda7a75895
David Czachor <david@czachor.dev>
2024-04-07 13:11:07 -0400

    Added try/catch

--- src/db/actions.ts
+++ src/db/actions.ts
@@ -13,29 +13,38 @@ export async function saveWallEntry({
 }: {
   user_message: string
 }) {
-  const { session } = await validateRequest()
-  if (!session) {
-    throw new Error('Unauthorized')
+  try {
+    const { session } = await validateRequest()
+
+    if (!session) {
+      throw new Error('Unauthorized')
+    }
+
+    await db.insert(wallEntries).values({
+      user_message,
+      user_id: session.userId,
+    })
+
+    revalidatePath('/wall')
+  } catch (error) {
+    throw new Error('Failed to save wall entry. Please try again later.')
   }
-
-  await db.insert(wallEntries).values({
-    user_message,
-    user_id: session.userId,
-  })
-
-  revalidatePath('/wall')
 }
 
 export async function getWallEntries() {
-  noStore()
-  return await db
-    .select({
-      id: wallEntries.id,
-      user_message: wallEntries.user_message,
-      user_name: userTable.user_name,
-      user_pic: userTable.user_pic,
-    })
-    .from(wallEntries)
-    .leftJoin(userTable, eq(wallEntries.user_id, userTable.id))
-    .orderBy(desc(wallEntries.id))
+  try {
+    noStore()
+    return await db
+      .select({
+        id: wallEntries.id,
+        user_message: wallEntries.user_message,
+        user_name: userTable.user_name,
+        user_pic: userTable.user_pic,
+      })
+      .from(wallEntries)
+      .leftJoin(userTable, eq(wallEntries.user_id, userTable.id))
+      .orderBy(desc(wallEntries.id))
+  } catch (error) {
+    throw new Error('Failed to fetch wall entries. Please try again later.')
+  }
 }
