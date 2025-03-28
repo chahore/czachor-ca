d955f2f56c4708842af5c19450e525f7ca93eaf9
David Czachor <david@czachor.ca>
2025-03-28 15:26:01 -0400

    Refactor project structure to migrate from Next.js to Astro.

--- src/components/global/site-header.tsx
+++
@@ -1,9 +0,0 @@
-import Nav from './nav';
-
-export default async function SiteHeader() {
-  return (
-    <header className="flex h-28 items-center bg-transparent pt-6">
-      <Nav />
-    </header>
-  );
-}
--- src/lib/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Newsreader as FontSerif } from 'next/font/google';
-
-export const fontSerif = FontSerif({
-  subsets: ['latin'],
-  style: ['italic'],
-  weight: ['400'],
-  variable: '--font-serif',
-});
