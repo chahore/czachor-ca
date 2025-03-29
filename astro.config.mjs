f8d65ee4ae4edb711b0e16ba5192172defd492c3
David Czachor <david@czachor.ca>
2025-03-28 22:13:06 -0400

    Implement site configuration and integrate Tailwind CSS with sitemap support. Remove unused files and restructure layout components for improved organization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import siteMap from '@astrojs/sitemap'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://czachor.ca',
+	integrations: [siteMap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
---
+++ src/site.config.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+	title: 'David Czachor',
+	description:
+		"I'm an electrician / software engineer who enjoys building things and keeping life simple.",
+	socials: [
+		{
+			title: 'email',
+			href: 'mailto:david@czachor.ca',
+		},
+		{
+			title: 'github',
+			href: 'https://github.com/chahore',
+		},
+		{
+			title: 'linkedin',
+			href: 'https://www.linkedin.com/in/davidczachor/',
+		},
+	],
+}
